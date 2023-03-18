9826e7b550f7ef8fe2f15319990806a03aecb816
yeboahstephen <yeboahstephen679@gmail.com>
2023-03-18 11:46:14 -0700

    alphabetical

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,18 +10,18 @@
 
 
 int main(void)
-        
-{       
-        
+
+{
+
         char low;
-        
+
         for (low = 'a'; low <= 'z'; low++)
         putchar(low);
-        
+
         for (low = 'A'; low <= 'Z'; low++)
         putchar(low);
         putchar('\n');
-        
+
         return (0);
 
 }
