eaa86ca7b08f8172459afe5ccc2cad277375cc54
yeboahstephen <yeboahstephen679@gmail.com>
2023-03-23 12:05:59 -0700

    compile with this main file to test locally

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - tests function that print if integer is poditive or negative
+ * Return: 0
+ */
+
+
+int main(void)
+
+{
+
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
