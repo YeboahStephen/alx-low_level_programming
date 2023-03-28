aeddc798cc013fce389b9feca6a4a9e39a405b76
yeboahstephen <yeboahstephen679@gmail.com>
2023-03-27 19:51:52 -0700

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,21 @@
 #include "main.h"
-
 /**
- * _puts -  prints a string
- * @str: The string to print
- * Return: void
- */
+*_puts - prints a string
+*@str: A pointer to an int that will be changed
+*
+*Return: void which means our answer is correct
+*/
 
 void _puts(char *str)
-
 {
-	for (; *str != '\0'; str++)
-	{
-	_putchar(*str);
-	}
-	_putchar('\n');
+char *c;
+int m;
+
+c = str;
+
+for (m = 0; c[m]; m++)
+{
+_putchar (c[m]);
+}
+_putchar('\n');
 }
