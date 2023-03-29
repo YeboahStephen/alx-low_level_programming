32b41bf048dab130b44ca9d29bebd0b0ba3064e9
yeboahstephen <yeboahstephen679@gmail.com>
2023-03-28 18:03:09 -0700

    A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,7 +18,7 @@ void rev_string(char *s)
 	for (index = len - 1; index >= len / 2; index--)
 	{
 	tmp = s[imdex];
-	s[index] = s[len - imdex - 1];
+	s[index] = s[len - index - 1];
 	s[len - index - 1] = tmp;
 	}
 }
