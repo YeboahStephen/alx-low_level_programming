27ffcf76f38a3b11fe3dc8df8eacfa53db5604d4
yeboahstephen <yeboahstephen679@gmail.com>
2023-04-04 20:11:42 -0700

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * ste_string - function that sets the value of a pointer to a char.
+ * @s: input 
+ * @to: input
+ * Return: Always 0 (Success)
+ */
+
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+}
