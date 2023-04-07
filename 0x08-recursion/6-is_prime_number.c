8844f6c47d18c5cecdd8563f85da5cbbce9d8c24
yeboahstephen <yeboahstephen679@gmail.com>
2023-04-06 23:13:20 -0700

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -16,7 +16,7 @@ int is_prime_number(int n)
 
 /**
  * check_prime - Check if number is prime
- * @n: the number to be checked 
+ * @n: the number to be checked
  * @i: the iteration times
  *
  * Return: 1 for prime or 0 composite
