5b48d8293128c32c680928ff976a1dbff730df0e
Yeboah Stephen <yeboahstephen679@gmail.com>
2023-04-29 17:13:25 -0700

    free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - A function that frees a listint_t list
+ * @head: Head of list
+ * Return: 0
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
